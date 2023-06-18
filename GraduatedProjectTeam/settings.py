04cae5c61851a7b9b7300b7652092e0ca9010687
alitamesh1 <130612310+alitamesh1@users.noreply.github.com>
2023-06-18 21:57:52 +0300

    change admin panel
    it is package

--- GraduatedProjectTeam/settings.py
+++ GraduatedProjectTeam/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'accounts.MyUsers'
 
 INSTALLED_APPS = [
+    'simpleui',   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -133,11 +134,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
